3879432df669945af0f1c683bd45283ec8ae4246
Surja2003 <surja2003@example.com>
2025-12-14 20:34:41 +0530

    Add password authentication to restrict access

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AuthGate from './components/AuthGate';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </StrictMode>
 );
 
